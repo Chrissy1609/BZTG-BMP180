d0b461a16519aa25fcd637ad1cb7b9ac58df2f87
Your Name <you@example.com>
2021-11-16 14:46:07 +0100

    Version_16.11.2021_14:46

---
+++ Ausgabe_Temperatur_Druck.py
@@ -0,0 +1,25 @@
+# Name   : Christian Meier
+# Klasse : ETS 2021
+# Datum  : 16.11.2021
+
+from bmp180 import BMP180
+from machine import Pin
+from machine import I2C
+import time 
+
+bus = I2C(scl = Pin(22), sda = Pin(21), freq = 100000)
+
+bmp180 = BMP180(bus)
+bmp180.oversample_sett = 2
+bmp180.baseline = 101325
+
+Temperatur = int(bmp180.temperature) 
+Luftdruck  = int(bmp180.pressure / 100) 
+errechneteHoehe = int(bmp180.altitude) 
+
+while True:
+    print(Temperatur, "\u00b0C")
+    print(Luftdruck, "hPa")
+    print(errechneteHoehe, "Meter ueber Wasser")
+    print("\n", "\n")
+    time.sleep(10)
