2df90ae967ba951ad716bfc191914a41b69f8f26
Chrissy1609 <91952546+Chrissy1609@users.noreply.github.com>
2021-12-13 16:43:35 +0100

    Kommentiert

--- Ausgabe_Temperatur_Druck.py
+++ Ausgabe_Temperatur_Druck.py
@@ -2,24 +2,23 @@
 # Klasse : ETS 2021
 # Datum  : 16.11.2021
 
-from bmp180 import BMP180
-from machine import Pin
-from machine import I2C
-import time 
+from bmp180 import BMP180                                           # Bibliothek Sensor (BMP180) Temperatur,Luftdruck
+from machine import Pin                                             # Bibliothek Pin (Ein/Ausgang)                                             
+from machine import I2C                                             # Bibliothek Busssystem
+import time                                                         # Bibliothek Zeiten
 
-bus = I2C(scl = Pin(22), sda = Pin(21), freq = 100000)
+bus = I2C(scl = Pin(22), sda = Pin(21), freq = 100000)              # Bussystem Parameter eingestellt
 
-bmp180 = BMP180(bus)
+bmp180 = BMP180(bus)                                                # Busssystem mit Sensor verbunden
 bmp180.oversample_sett = 2
 bmp180.baseline = 101325
+Temperatur = int(bmp180.temperature)                                # Wert der Temperatur in Ganzenwerte ausgegeben
+Luftdruck  = int(bmp180.pressure / 100)                             # Wert des Luftdruck in Ganzenwerte ausgegeben
+errechneteHoehe = int(bmp180.altitude)                              # Wert der Höhe über Null in Ganzen Werten
 
-Temperatur = int(bmp180.temperature) 
-Luftdruck  = int(bmp180.pressure / 100) 
-errechneteHoehe = int(bmp180.altitude) 
-
-while True:
-    print(Temperatur, "\u00b0C")
-    print(Luftdruck, "hPa")
-    print(errechneteHoehe, "Meter ueber dem Wasser")
-    print("\n")
-    time.sleep(10)
+while True:                                                         # Wiederholende Schleife
+    print(Temperatur, "\u00b0C")                                    # Wert Temperatur ausgegeben, Unicode für ° 
+    print(Luftdruck, "hPa")                                         # Wert Luftdruck ausgegeben
+    print(errechneteHoehe, "Meter ueber dem Wasser")                # Wert Höhe ausgegeben
+    print("\n")                                                     # Leerzeichen ausgeben 
+    time.sleep(10)                                                  # Wartezeit von 10 Sekunden
